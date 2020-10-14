f6da59858be776705401d56dc1dd2c3e943f4c16
a.amangeldiev <kuu.life@gmail.com>
2020-10-14 13:38:21 +0600

    working with images with python examples

---
+++ test1.py
@@ -0,0 +1,45 @@
+# #import Image from PILLOW
+# from PIL import Image
+
+# #creating image variable
+# pic = Image.open('pic.jpg')
+# pic2 = Image.open(pic2.jpg)
+
+# #printing size of images
+# print(pic.size
+# print(pic2.size)
+
+# show images
+# pic.show()
+# pic2.show()
+
+# cropping image to the size that you want
+# pic.crop((0,0,100,100))
+# pic2.crop((0,0,100,100))
+
+# x= 0,y= 0
+# w = 3141/3,h = 4712/10
+
+#cropping image with variables that you assigned
+#pic2.crop((x,y,width,height))
+
+#printing format description of image
+# print(pic.format_description)
+# print(pic.format_description)
+
+#printing image name
+# print(pic.filename)
+# print(pic2.filename)
+
+#pasting pic2 to the pic image
+# pic.paste(pic2,(0,0),pic2)
+
+# resizing image
+# pic.resize((1000,1000))  
+
+# pic.rotate(90) rotate image 90 degree
+# pic.putalpha(100)  transparency 100
+
+#put transparency and make new file
+# p = pic.putalpha(20)
+# p.save("/user/macbook/desktop/newpic.png")
